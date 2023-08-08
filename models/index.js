c8c7c30be400a3d68421161ffd2496dae71c39f6
moonjunsain <moonjunsain@gmail.com>
2023-08-07 17:47:43 -0700

    established a relationship between models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,44 @@
 const Comment = require('./Comment')
 const User = require('./User')
 const Blog = require('./Blog')
+
+// User can have many blog posts
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+// blog belongs to one user
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+// comment belongs to one user 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+
+
+// Blog can have many comments
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+})
+
+// comment belongs to one blog
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+})
+
+
+
+module.exports = {
+    Comment, User, Blog
+}
